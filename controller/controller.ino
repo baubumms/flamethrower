21044a9e40cac170f40ffde601e3cc34c34eb5b8
LeoLurch <40560457+leolurch@users.noreply.github.com>
2021-04-09 16:46:40 +0200

    comments

--- controller/controller.ino
+++ controller/controller.ino
@@ -5,14 +5,16 @@
 #define VALVE_PIN 6
 
 #define MAX_THROWING_TIME 5000 //max duration in ms
-#define ARC_IGNITION_TIME 500
+//#define ARC_IGNITION_TIME 500
 
+//triigger states
 bool state = false;
 bool lastState = false;
 
 bool arcing = false;
 bool firing = false;
 
+//timestamps of individual actions to stop asyncly after timeout
 long fireTime = 0;
 long arcTime = 0;
 
@@ -22,12 +24,11 @@ void setup() {
   #endif
   
   pinMode(TRIGGER_PIN, INPUT_PULLUP);
-  groundPin(3);
+  groundPin(3);//ground for trigger button (didn't want to solder to many wires to one gnd pin -> remove if you can solder)
   pinMode(ARC_PIN, OUTPUT);
   pinMode(VALVE_PIN, OUTPUT);
-  groundPin(5);
 
-  attachInterrupt(digitalPinToInterrupt(TRIGGER_PIN), triggerChange, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(TRIGGER_PIN), triggerChanged, CHANGE);//call callback every time the trigger gets activated
 }
 
 void loop() {
@@ -35,37 +36,39 @@ void loop() {
   checkTimers();
 }
 
-void checkState(){
-  //was the trigger triggered?
+void checkState(){//react if state changed
+  //trigger triggered?
   if(state == lastState)
     return;
 
   if(!state)
     stopFire();
   else{
+    //filter out weird behaviour when trigger button is in transition between triggered and not triggered state
     delay(3);
-    if(digitalRead(TRIGGER_PIN) == HIGH){
+    if(digitalRead(TRIGGER_PIN) == HIGH)
       state = false;
-      return;
-    }
-    startFire();
+    else
+      startFire();
   }
     
 
   lastState = state;
 }
 
-void checkTimers(){
+void checkTimers(){//react if time limits reached
   //asynchronosly turn of fire extinguishing after delay
   if(millis() - fireTime >= MAX_THROWING_TIME && state && firing)
     stopFire();
 
-  //asynchronosly turn of arc iginition extinguishing after delay
+#ifdef ARC_IGNITION_TIME 
+  //asynchronosly turn of arc iginition after delay
   if( millis() - arcTime >= ARC_IGNITION_TIME && state && arcing)
     stopArc();
+#endif
 }
 
-void triggerChange(){
+void triggerChanged(){//callback for interrupt on change of trigger 
   if(digitalRead(TRIGGER_PIN) == LOW)
     state = true;
   else
@@ -106,7 +109,7 @@ void stopArc(){
   #endif
 }
 
-void groundPin(int pin){
+void groundPin(int pin){//connects pin to ground - only capable for dealing with low currents
   pinMode(pin, OUTPUT);
   digitalWrite(pin, LOW);
 }
